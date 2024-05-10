7442d0ed65b6704bb33003a8873f7ae5cd7dd2a0
davidcoats <david@getsafetycone.com>
2024-05-10 00:07:00 -0700

    Interim changes.

--- source/R5T.H0001/Code/Entities/Project.cs
+++ source/R5T.H0001/Code/Entities/Project.cs
@@ -7,7 +7,8 @@ using R5T.T0249;
 namespace R5T.H0001.Entities
 {
 	[EntityMarker]
-	public class Project : IEntityMarker
+	public class Project : IEntityMarker,
+		IProjectDescriptor
 	{
 		[Key]
 		public int ID { get; set; }
@@ -18,6 +19,7 @@ namespace R5T.H0001.Entities
 		// Will be an alternate key.
 		public int? ProjectFilePathID { get; set; }
 		public ProjectFilePath ProjectFilePath { get; set; }
+		string IProjectDescriptor.FilePath => this.ProjectFilePath.Value;
 
 		public string Name { get; set; }
         public string Description { get; set; }
@@ -28,5 +30,6 @@ namespace R5T.H0001.Entities
 		
 		public int? ProjectReferencesListID { get; set; }
 		public ProjectFilePathList ProjectReferencesList { get; set; }
-	}
+
+    }
 }
---
+++ source/R5T.H0001/Code/_Types/Interfaces/IProjectDescriptor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace R5T.H0001
+{
+    public interface IProjectDescriptor
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public string Type { get; }
+
+        public string FilePath { get; }
+
+        public bool IsPrivate { get; }
+    }
+}
