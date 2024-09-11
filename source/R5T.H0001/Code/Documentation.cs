4a5ce5562923641f04b91bdceb60d4b8bbc1c46a
davidcoats <david@getsafetycone.com>
2024-09-11 13:18:02 -0700

    Interim commit.

--- source/R5T.H0001/Code/Documentation.cs
+++ source/R5T.H0001/Code/Documentation.cs
@@ -3,10 +3,14 @@ using System;
 
 namespace R5T.H0001
 {
-	/// <summary>
-	/// Projects repository database.
-	/// </summary>
-	public static class Documentation
+    /// <summary>
+    /// Projects repository database.
+    /// </summary>
+    /// <remarks>
+    /// <see cref="T0249.Documentation"/>
+    /// <see cref="T0250.Documentation"/>
+    /// </remarks>
+    public static class Documentation
 	{
 	}
 }
